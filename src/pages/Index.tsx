7930320559dd47b93fb0ebce6a1d6fa3f9e1ef16
TheMobileProf <samuelanyaele@gmail.com>
2025-05-28 12:52:23 +0100

    small changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -15,7 +15,7 @@ const Index = () => {
       <HowItWorks />
       <Services />
       <Booking />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <FAQ />
       <Footer />
     </div>
